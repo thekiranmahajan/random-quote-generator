606c25c4ff800106f197d78c00cf5dd2f126bdb5
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-03 22:04:23 +0530

    Done✅🥳🥳

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const quote = document.getElementById("quote");
-const author = document.getElementById("author");
 const btn = document.getElementById("btn");
+const getRandomQuote = async () => {
+  const url = `https://api.api-ninjas.com/v1/quotes?`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-Api-Key": "238YFDFM830qHpo7wMJT9Q==5P90c45LkLGt2m4U",
+    },
+  };
+  try {
+    btn.disabled = true;
+    quote.innerText = "Soon a quote will appear here🫷🏻...";
+    author.innerText = "~ fetching";
+    category.innerText = "{ 😅 }";
+    const data = await fetch(url, options).then((res) => res.json());
+
+    quote.innerText = data[0].quote;
+    author.innerText = "~ " + data[0].author;
+    category.innerText = "{" + data[0].category + "}";
+    // console.log(data[0].category);
+    btn.disabled = false;
+  } catch (error) {
+    btn.disabled = false;
+    quote.innerText = "Something went wrong...";
+    author.innerText = "~ ";
+    category.innerText = "not Ray's fault 😅";
+  }
+};
+getRandomQuote();
+btn.addEventListener("click", getRandomQuote);
